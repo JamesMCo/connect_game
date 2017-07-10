b60e654158d1ad8161f4efde28d8035a74d57137
James C <3661681+JamesMCo@users.noreply.github.com>
2017-07-10 15:12:01 +0100

    Added game with some questions

---
+++ data.js
@@ -0,0 +1,14 @@
+questions = [
+["He's my brother", "Well, nobody's perfect", "Mein Fuhrer, I can walk", "It was Beauty killed the Beast", "Last lines in movies<br>(Return of the Jedi, Some Like It Hot, Dr. Strangelove, King Kong)", "connect"],
+["Epistle to the Ephesians", "De Profundis", "Don Quixote", "Pilgrim’s Progress", "Written in prison<br>(St. Paul, Oscar Wilde, Cervantes, John Bunyan)", "connect"],
+["Laconicum", "Caldarium", "Tepidarium", "Frigidarium", "Bathing rooms at a Roman baths, from hottest to coldest", "sequence"],
+["Herbert", "Jefferson", "Walker", "Hussein", "Second names of U.S. Presidents<br>George Herbert Bush, William Jefferson Clinton, George Walker Bush Junior, Barack Hussein Obama", "sequence"],
+["Chia-Chia and Ching-Ching", "100,000 tulip bulbs", "The Statue of Liberty", "Trafalgar Square Christmas Tree", "Gifts given from one country to another", "connect"],
+["Bohemian Rhapsody", "Tosca", "Delilah", "I Shot the Sheriff", "Confessing to murder", "connect"],
+["LBQ", "News from Elsewhere", "10 things we didn't know last week", "7 days<br>7 questions", "BBC News Magazine", "connect"],
+["<img src='img/taxis/1.png' style='width: 100%; height: auto;'>", "<img src='img/taxis/2.png' style='width: 100%; height: auto;'>", "<img src='img/taxis/3.png' style='width: 100%; height: auto;'>", "<img src='img/taxis/4.png' style='width: 100%; height: auto;'>", "Taxi colours", "connect"],
+["<span style='color: #0575c9; font-size: 3em;'>Tree</span>", "<span style='color: #ed1e0e; font-size: 3em;'>Rose</span>", "<span style='color: #f7db15; font-size: 3em;'>Ribbon</span>", "<span style='color: #e56600; font-size: 3em;'>Bird</span>", "UK Political Parties in 2017 General Election", "sequence"],
+["Netherlands: Epsom salts", "Italy: trifle", "France: custard sauce", "USA: soft white muffin", "Other nationalities call them 'English'", "connect"],
+["9th: 'Run.'", "10th: 'Hello. OK. New teeth. That's weird.'", "11th: 'Legs! I've still got legs!'", "12th: 'Kidneys! I've got new kidneys!'", "Doctor Who first lines", "connect"],
+["The arrival of Captain James Cook", "Centaur throwing a spear of light", "2,008 Fou drummers", "(e.g.) Queen jumps from a helicopter", "Olympic opening ceremonies", "sequence"]
+]
---
+++ js.js
@@ -0,0 +1,150 @@
+var current_question = [];
+current_state = 0;
+// 0 -> Hieroglyph Selection
+// 1 -> Hieroglyph Chosen, Input Blocked
+// 2 -> Question on Screen, Card 1 Shown
+// 3 -> Question on Screen, Card 2 Shown
+// 4 -> Question on Screen, Card 3 Shown
+// 5 -> Question on Screen, All Cards Shown
+// 6 -> Question on Screen, All Cards Shown, Link Revealed
+
+document.onkeypress = function (e) {
+  e = e || window.event;
+  if ((current_state == 0) || (current_state == 1)) {
+    if ((e.key == "a") && ($("#hiero_a").hasClass("played") == false)) {
+      $(".chosen").removeClass("chosen");
+      $("#hiero_a").addClass("chosen");
+      current_state = 1;
+    }
+    else if ((e.key == "b") && ($("#hiero_b").hasClass("played") == false)) {
+      $(".chosen").removeClass("chosen");
+      $("#hiero_b").addClass("chosen");
+      current_state = 1;
+    }
+    else if ((e.key == "c") && ($("#hiero_c").hasClass("played") == false)) {
+      $(".chosen").removeClass("chosen");
+      $("#hiero_c").addClass("chosen");
+      current_state = 1;
+    }
+    else if ((e.key == "d") && ($("#hiero_d").hasClass("played") == false)) {
+      $(".chosen").removeClass("chosen");
+      $("#hiero_d").addClass("chosen");
+      current_state = 1;
+    }
+    else if ((e.key == "e") && ($("#hiero_e").hasClass("played") == false)) {
+      $(".chosen").removeClass("chosen");
+      $("#hiero_e").addClass("chosen");
+      current_state = 1;
+    }
+    else if ((e.key == "f") && ($("#hiero_f").hasClass("played") == false)) {
+      $(".chosen").removeClass("chosen");
+      $("#hiero_f").addClass("chosen");
+      current_state = 1;
+    }
+  }
+  if (current_state == 1) {
+    if (e.key == "n") {
+      $("#hiero").fadeOut(750);
+      $("#question_a").hide();
+      $("#question_b").hide();
+      $("#question_c").hide();
+      $("#question_d").hide();
+      $("#question_l").hide();
+
+      load_question();
+      if (current_question[5] == "connect") {
+        $(".sequence").removeClass("sequence");
+        $("#question_d").html("<h1>" + current_question[3] + "</h1>");
+      }
+      else if (current_question[5] == "sequence") {
+        $("#question_a").addClass("sequence");
+        $("#question_b").addClass("sequence");
+        $("#question_c").addClass("sequence");
+        $("#question_d").addClass("sequence");
+        $("#question_l").addClass("sequence");
+
+        $("#question_d").html("<h1>?</h1>");
+      }
+
+      $("#question_a").html("<h1>" + current_question[0] + "</h1>");
+      $("#question_b").html("<h1>" + current_question[1] + "</h1>");
+      $("#question_c").html("<h1>" + current_question[2] + "</h1>");
+      $("#question_l").html("<h1>" + current_question[4] + "</h1>");
+
+      $("#question").delay(1000).show();
+      $("#question_a").delay(1250).fadeIn();
+      current_state = 2;
+    }
+  }
+  else if (current_state == 2) {
+    if (e.key == "n") {
+      $("#question_b").fadeIn().css("display", "flex");
+      current_state = 3;
+    }
+    else if (e.key == "q") {
+      $("#question_b").fadeIn().css("display", "flex");
+      $("#question_c").fadeIn().css("display", "flex");
+      $("#question_d").fadeIn().css("display", "flex");
+
+      current_state = 5;
+    }
+  }
+  else if (current_state == 3) {
+    if (e.key == "n") {
+      $("#question_c").fadeIn().css("display", "flex");
+      if (current_question[5] == "connect") {
+        current_state = 4;
+      }
+      else if (current_question[5] == "sequence") {
+        $("#question_d").fadeIn().css("display", "flex");
+        current_state = 5;
+      }
+    }
+    else if (e.key == "q") {
+      $("#question_b").fadeIn().css("display", "flex");
+      $("#question_c").fadeIn().css("display", "flex");
+      $("#question_d").fadeIn().css("display", "flex");
+
+      current_state = 5;
+    }
+  }
+  else if (current_state == 4) {
+    if ((e.key == "n") || (e.key == "q")) {
+      $("#question_d").fadeIn().css("display", "flex");
+      current_state = 5;
+    }
+  }
+  else if (current_state == 5) {
+    if (e.key == "n") {
+      $("#question_l").fadeIn();
+      if (current_question[5] == "sequence") {
+        $("#question_d").html("<h1>" + current_question[3] + "</h1>")
+      }
+      current_state = 6;
+    }
+  }
+  else if (current_state == 6) {
+    if (e.key == "n") {
+      $("#question").fadeOut(750);
+
+      $(".chosen").addClass("played");
+      $(".chosen").removeClass("chosen");
+
+      $("#hiero").delay(1000).fadeIn(750);
+      current_state = 0;
+    }
+  }
+  if (e.key == "r") {
+  $(".played").removeClass("played");
+  }
+}
+
+function load_question() {
+  temp = questions.splice(Math.floor(Math.random() * questions.length), 1)[0];
+  if (temp !== undefined) {
+    current_question = temp;
+  }
+  else {
+    alert("Out of questions");
+  }
+}
